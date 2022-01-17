d07225805b929710a66e603c04d10a6f390c02af
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-17 13:02:48 -0800

    add NavBar Component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <header>
+            <div className='navbar navbar-dark bg-dark box-shadow'>
+                <div className='container d-flex justify-content-between'>
+                    <div className='text-warning'>
+                        <h2>
+                            <strong>Fetch Pokemon API</strong>
+                        </h2>
+                    </div>
+                    <div>
+                        <button className='btn btn-secondary round'><strong>Home</strong></button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default NavBar
