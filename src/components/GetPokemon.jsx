1fdeaec119f80660f9700b59abc6b226fa6f1890
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-17 19:47:43 -0800

    fetch all pokemon using a button

--- src/components/GetPokemon.jsx
+++ src/components/GetPokemon.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 /**
  * /////////////////////////////////////////////////////////////////////
@@ -7,13 +7,30 @@ import React from 'react'
  *  * @returns A Button which will fetch pokemo 
  */
 const GetPokemon = () => {
+    // //// FIELDS /////////////////////////////////////////////////////
+    const [pokemonList, setPokemonList] = useState([]);
+
+
+    const fetchPokemon = (e) => {
+        e.preventDefault();
+        console.log("*****************");
+        console.log("In Fetch Pokemon");
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
+            .then(response => response.json())
+            .then(response => setPokemonList(response.results));
+    }
+
+    // //// OUTPUT /////////////////////////////////////////////////////
     return (
         <div>
-            <form className="row">
+            <form className="row"
+                onSubmit={(e) => fetchPokemon(e)} >
                 <button className="col btn btn-primary round">
                     <strong>Fetch Pokemon</strong>
                 </button>
             </form>
+            <h3>Pokemon List</h3>
+            <p>{JSON.stringify(pokemonList)}</p>
         </div>
     )
 }
