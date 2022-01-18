8e0456d6b35892a597b9761da81a678febd31fe6
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-17 19:22:49 -0800

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Main from './components/Main';
 
 function App() {
   return (
---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+/**
+ * ////////////////////////////////////////////////////////
+ *      MAIN COMPONENT
+ * ////////////////////////////////////////////////////////
+ */
+const Main = () => {
+    return (
+        <main role="main">
+            <div className='container mt-4'>
+                <div className='row border'>
+                    <h1>In Main</h1>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Main
